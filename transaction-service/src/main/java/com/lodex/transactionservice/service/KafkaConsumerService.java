98b4f5fdf10924774ba4b51158595617068c95c6
Ahmed Tharwat <ahmedlordexg@gmail.com>
2026-04-11 23:23:35 +0200

    fix: modified kafka service annotation

--- transaction-service/src/main/java/com/lodex/transactionservice/service/KafkaConsumerService.java
+++ transaction-service/src/main/java/com/lodex/transactionservice/service/KafkaConsumerService.java
@@ -1,20 +1,16 @@
 package com.lodex.transactionservice.service;
 
 import com.lodex.transactionservice.model.entity.Transaction;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
 
 @Service
-@Slf4j
+@RequiredArgsConstructor
 public class KafkaConsumerService {
 
     private final ObjectMapper objectMapper;
-
-    public KafkaConsumerService(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
 
     @KafkaListener(topics = "transaction-topic", groupId = "transaction-group")
     public void listen(String transactionStr) {
