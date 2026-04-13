ab0dc74b79dc04be7716c122118bc9f513b2fb7f
Abdelrhman Mohsen <abdulrahman_20230317@fci.helwan.edu.eg>
2026-04-13 16:29:16 +0200

    Implement error handling in user-service

---
+++ user-service/src/main/java/com/core/userservice/controller/ErrorController.java
@@ -0,0 +1,68 @@
+package com.core.userservice.controller;
+
+import com.core.userservice.domain.APIErrorResponse;
+import com.core.userservice.exception.UserForbiddenException;
+import com.core.userservice.exception.UserUnauthorizedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<APIErrorResponse> handleException(Exception ex) {
+        log.error("Caught exception", ex);
+        APIErrorResponse error = APIErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("Internal Server Error")
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<APIErrorResponse> handleHttpMessageNotReadableException(Exception ignoredEx) {
+        APIErrorResponse error = APIErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("The server cannot process the request due to client error")
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<APIErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        APIErrorResponse error = APIErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("The server cannot process the request due to client error")
+                .errors(ex.getBindingResult().getAllErrors().stream()
+                        .map(e -> new APIErrorResponse.FieldError(((FieldError) e).getField(), e.getDefaultMessage()))
+                        .toList()
+                )
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserUnauthorizedException.class)
+    public ResponseEntity<APIErrorResponse> handleUserUnauthorizedException(UserUnauthorizedException ignoredEx) {
+        APIErrorResponse error = APIErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message("Incorrect Credentials")
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(UserForbiddenException.class)
+    public ResponseEntity<APIErrorResponse> handleUserForbiddenException(UserForbiddenException ignoredEx) {
+        APIErrorResponse error = APIErrorResponse.builder()
+                .status(HttpStatus.FORBIDDEN.value())
+                .message("Insufficient Permissions")
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+    }
+}
